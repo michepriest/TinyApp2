51b1e51098e5d3d412e4fa0e1c3dfb4f6f788db9
michepriest <miche.priest@gmail.com>
2018-10-03 00:38:49 +0000

    added routes for new urls and urls files

---
+++ express_server2.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const express = require("express");
+const app = express();
+let PORT = 8080;
+
+app.set("view engine", "ejs");
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+// must go above app.get("/urls/:id", ...)
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
